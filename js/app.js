26fb4bb664464907192d2509ee8ee38fcae0c56a
yanHuang <yanhuang2013@gmail.com>
2018-10-27 17:34:31 -0400

    refactor the sun plants and zombie object, update new assets

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-let gameState = {
-  sunCount: 0,
-  isZombieWin: false,
-  isSunExist: true,
-  plantArr: [],
-  readyPlant: {},
-  bullets: [],
-  zombiesArr: []
-}
 
-
+// let gameState = {
+//   sunCount: 0,
+//   isZombieWin: false,
+//   isSunExist: true,
+//   plantArr: [],
+//   readyPlant: {},
+//   bullets: [],
+//   zombiesArr: []
+// }
+// function to create a peashooter object
 function Peashooter() {
   // peashooter initialization
   this.plant = this.init();
@@ -20,9 +20,9 @@ function Peashooter() {
 }
 // peashooter prototype initialization
 Peashooter.prototype.init = function() {
-  let peashooter = document.createElement("div");
-  peashooter.innerHTML = '<img src="assets/peashooter.gif">';
-  return peashooter;
+    let peashooter = document.createElement("div");
+    peashooter.innerHTML = '<img src="assets/peashooter.gif">';
+    return peashooter;
 };
 // place the plant in which position
 Peashooter.prototype.placePlant = function(left) {
@@ -78,7 +78,7 @@ Peashooter.prototype.shoot = function(zombiesG) {
           pod.src = "assets/podHit.gif";
           setTimeout(function() {
             if (pod.parentNode != zombiesContainer) return;
-            zombiesContainer.removeChild(bullet);
+            zombiesContainer.removeChild(pod);
           }, 260);
           if (zombie.vitality >= 0) {
             zombie.vitality -= 1;
@@ -174,7 +174,6 @@ Peashooter.prototype.shoot = function(zombiesG) {
   this.podFly = setInterval(generateShoot, self.shootTime);
 };
 
-
 // function to stop the peashooter from shooting
 Peashooter.prototype.stopShoot = function() {
   clearInterval(this.podFly);
@@ -196,50 +195,50 @@ function Zombie() {
 
 }
 Zombie.prototype.init = function() {
-  let zombie = document.createElement('div');
-  zombie.style.left = this.left + 'px';
-  zombie.innerHTML = `<img src='assets/zombie-01.gif'>`;
-  zombiesContainer.appendChild(zombie);
-  return zombie;
+  let zombieEle = document.createElement('div');
+  zombieEle.style.left = this.left + 'px';
+  zombieEle.innerHTML = `<img src='assets/zombie-01.gif'>`;
+  zombiesContainer.appendChild(zombieEle);
+  return zombieEle;
 
 }
 // zombie walking function
 Zombie.prototype.walk = function() {
   let self = this;
-  self.zombieWalk = setInterval(function() {
-    self.zombie.style.left = self.zombie.offsetLeft - 1 + "px";
-    if (self.zombie.offsetLeft < -100) {
-      if (gameEnd) return;
-      gameOver();
-    }
+  self.zombieWalk = setInterval(function () {
+      self.zombie.style.left = self.zombie.offsetLeft - 1 + "px";
+      if (self.zombie.offsetLeft < -100) {
+          if (gameEnd) return;
+          gameOver();
+      }
   }, self.walkSpeed);
   let img = self.zombie.querySelector("img");
   img.src = "assets/zombie-01.gif";
 
 };
-Zombie.prototype.noHeadWalk = function() {
-  let self = this;
-  self.zombieWalk = setInterval(function() {
-    self.zombie.style.left = self.zombie.offsetLeft - 1 + "px";
-    if (self.zombie.offsetLeft < -100) {
-      if (gameEnd) return;
-      gameOver();
-    }
-  }, self.walkSpeed);
-  let img = self.zombie.querySelector("img");
-  img.src = "assets/zombieLoseHead.gif";
+Zombie.prototype.noHeadWalk = function () {
+    let self = this;
+    self.zombieWalk = setInterval(function () {
+        self.zombie.style.left = self.zombie.offsetLeft - 1 + "px";
+        if (self.zombie.offsetLeft < -100) {
+            if (gameEnd) return;
+            gameOver();
+        }
+    }, self.walkSpeed);
+    let img = self.zombie.querySelector("img");
+    img.src = "assets/zombieNoHeadWalk.gif";
 
 };
-Zombie.prototype.loseHead = function() {
-  let head = document.createElement("img");
-  head.src = "assets/zombie-01-head.gif";
-  head.className = "zombieHead";
-  head.style.left = self.zombie.offsetLeft + "px";
-  head.style.top = self.zombie.offsetTop + "px";
-  zombiesContainer.appendChild(head);
-  setTimeout(function() {
-    zombiesContainer.removeChild(head);
-  }, 1000);
+Zombie.prototype.loseHead = function () {
+    let head = document.createElement("img");
+    head.src = "assets/zombie-01-head.gif";
+    head.className = "zombieHead";
+    head.style.left = self.zombie.offsetLeft + "px";
+    head.style.top = self.zombie.offsetTop + "px";
+    zombiesContainer.appendChild(head);
+    setTimeout(function () {
+        zombiesContainer.removeChild(head);
+    }, 1000);
 };
 // zombie stop walking
 Zombie.prototype.stopWalk = function() {
@@ -267,111 +266,137 @@ Zombie.prototype.die = function(zombieDeadGroup) {
   zombieDeadGroup.push(true);
   zombiesContainer.removeChild(this.zombie);
 }
+// zombie wins
+let gameEnd;
+let gameOver = function() {
+  gameEnd = true;
+  let endingMessage = document.createElement('img');
+  endingMessage.src = "assets/gameOver.jpg"
+  endingMessage.className = "ZombieWinsMessage"
+  background.appendChild(endingMessage);
+  setTimeout(function() {
+    window.alert('Game is Over, Zombie wins')
+  }, 2800);
+};
 
 
-let sunCountEle = document.querySelector('#suncount');
-let backgroundEle = document.querySelector('.background');
-let gameConsoleEle = document.querySelector('.game-console');
-let peaConsoleEle = document.querySelector('#peashooter');
-let walnutConsoleEle = document.querySelector('#walnut');
-let sodPathEle = document.querySelector('.sod');
+  let backgroundEle = document.querySelector('.background');
+  let sodPathEle = document.querySelector('.sod');
+  let zombiesContainer = document.querySelector('.zombies-container');
+  let plantsContainer = document.querySelector('.plants-container');
+  let sunCountEle = document.querySelector('#suncount');
+  let gameConsoleEle = document.querySelector('.game-console');
+  let peaConsoleEle = document.querySelector('#peashooter');
+  let walnutConsoleEle = document.querySelector('#walnut');
+  let bgImageEle = document.querySelector('#bgImage');
 
-let createSuns = function() {
+  let totalZombies = 12;
+  // current state of zombies
+  let currentZombies = [];
+  // zombies already show
+  let zombiesGroup = [];
+  // all of zombies
+  let zombiesTotal = [];
+  // keep on updating the zombie states
+  let newZombiesGroup;
+  //record the zombies stopped walking
+  let zombieStoppedGroup = [];
+  // record dead zombies
+  let zombieDeadGroup = [];
+
+function Sun() {
+  this.sun = this.create();
+  this.value = 50;
+  this.existingTime = 3000;
+}
+Sun.prototype.create = function() {
   let sunEle = document.createElement('img');
-  sunEle.className = "sun";
+  sunEle.className = 'sun';
   sunEle.src = "./assets/sun.gif";
   backgroundEle.appendChild(sunEle);
-  gameState.isSunExist = true;
-  let sunLeft = Math.floor(Math.random() * (backgroundEle.offsetWidth - sunEle.offsetWidth - 280) + 200);
-  sunEle.style.left = sunLeft + 'px';
-  let sunTop = Math.floor(Math.random() * (backgroundEle.offsetHeight - sunEle.offsetHeight - 100) + 50);
-  sunEle.style.top = '10px';
-  let sunFallTimer = setInterval(function() {
-    if (sunEle.offsetTop > sunTop) {
-      clearInterval(sunFallTimer);
-      if (sunEle.offsetTop > 360 || sunEle.offsetTop < 240) {
-        setTimeout(function() {
-          if (gameState.isSunExist) {
-            sunEle.style.left = "-60px";
-            sunEle.style.top = "-60px";
-            backgroundEle.removeChild(sunEle);
-          }
-        }, 3000);
-      }
-    } else {
-      sunEle.style.top = sunEle.offsetTop + 16 + 'px';
-    }
-  }, 20);
-  // sunEle.style.top = sunTop + 'px';
+  return sunEle;
 }
 
-let createPlants = function() {
-  let plant = document.createElement('img');
-  plant.className = 'plant';
-  plant.style.left = '0px';
-  plant.style.top = '4px';
-  return plant;
+Sun.prototype.dropSun = function() {
+  let sunLeft = Math.floor(Math.random() * (backgroundEle.offsetWidth - sunEle.offsetWidth - 280) + 200);
+  this.sun.style.left = sunLeft + 'px';
+  let sunTop = Math.floor(Math.random() * (backgroundEle.offsetHeight - sunEle.offsetHeight - 100) + 50);
+  this.sun.style.top = '10px';
+  let sunFallTimer = setInterval(function() {
+    if (this.sun.offsetTop > sunTop) {
+      clearInterval(sunFallTimer);
+      if (this.sun.offsetTop > 360 || this.sun.offsetTop < 240) {
+        setTimeout(function() {
+            this.sun.style.left = "-60px";
+            this.sun.style.top = "-60px";
+            backgroundEle.removeChild(this.sun);
+        }, this.existingTime);
+      }
+     }
+     else {
+       this.sun.style.top = this.sun.offsetTop + 16 + 'px';
+     }
+    }, 20);
 }
+
 let selectPlant = function(evt) {
   if (gameState.sunCount < 50) {
     walnutConsoleEle.id = 'walnut';
     peaConsoleEle.id = 'peashooter';
-  } else if (gameState.sunCount < 100) {
+  } else if(gameState.sunCount < 100) {
     walnutConsoleEle.id = 'walnut-active';
     peaConsoleEle.id = 'peashooter';
-  } else if (gameState.sunCount >= 100) {
+  } else if(gameState.sunCount >= 100) {
     peaConsoleEle.id = "peashooter-active";
     walnutConsoleEle.id = "walnut-active"
   }
 
   if (evt.target.id === "peashooter-active") {
-    gameState.sunCount -= 100;
-    sodPathEle.addEventListener('click', placePlant);
-    gameState.readyPlant = createPlants();
-    gameState.readyPlant.src = "./assets/peashooter.gif";
-    gameState.readyPlant.dataset.attack = 2;
-    gameState.readyPlant.dataset.speed = 50;
-    sunCountEle.innerText = gameState.sunCount;
-    if (gameState.sunCount < 50) {
+    sodPathEle.addEventListener('click',placePlant);
+    gameState.readyPlant = "peashooter";
+
+    if(gameState.sunCount < 50) {
       walnutConsoleEle.id = 'walnut';
       peaConsoleEle.id = "peashooter";
-    } else if (gameState.sunCount < 100) {
+    }else if(gameState.sunCount < 100) {
       evt.target.id = "peashooter";
     }
   } else if (evt.target.id === 'walnut-active') {
     gameState.sunCount -= 50;
     sodPathEle.addEventListener('click', placePlant);
-    gameState.readyPlant = createPlants();
-    gameState.readyPlant.src = 'assets/plant-walnut.png'
-    gameState.readyPlant.dataset.attack = 0;
+    gameState.readyPlant = 'walnut';
     sunCountEle.innerText = gameState.sunCount;
     if (gameState.sunCount < 50) {
       evt.target.id = "walnut";
       peaConsoleEle.id = "peashooter";
-    } else if (gameState.sunCount < 100) {
+    } else if(gameState.sunCount < 100) {
       peaConsoleEle.id = 'peashooter';
     }
   }
 }
 
+let gameState = {
+  sunCount: 0,
+  readyPlant: '';
+}
+
 let collectSun = function(evt) {
-  if (evt.target.className === "sun") {
-    gameState.isSunExist = false;
+  if(evt.target.className === "sun") {
     gameState.sunCount += 50;
     sunCountEle.innerText = gameState.sunCount;
 
     if (gameState.sunCount >= 100) {
-      peaConsoleEle.id = "peashooter-active";
-    } else if (gameState.sunCount >= 50) {
-      walnutConsoleEle.id = "walnut-active";
+        peaConsoleEle.id = "peashooter-active";
+    } else if(gameState.sunCount >= 50) {
+        walnutConsoleEle.id = "walnut-active";
     }
     let sunGoneTimer = setInterval(function() {
       if (evt.target.offsetLeft < 0 && evt.target.offsetTop < 0) {
         evt.target.parentNode.removeChild(evt.target);
         clearInterval(sunGoneTimer);
       } else {
-        evt.target.style.left = evt.target.offsetLeft - 24 + 'px';
-        evt.target.style.top = evt.target.offsetTop - 24 + 'px';
+        evt.target.style.left = evt.target.offsetLeft - 20 + 'px';
+        evt.target.style.top = evt.target.offsetTop - 20 + 'px';
       }
     }, 20);
   }
@@ -379,91 +404,33 @@ let collectSun = function(evt) {
 
 let placePlant = function(evt) {
   if (evt.offsetX > 30 && evt.offsetX + 20 < this.offsetWidth) {
-    if (gameState.readyPlant) {
-      let plant = gameState.readyPlant;
-      plant.style.left = evt.offsetX - 48 + 'px';
-      sodPathEle.appendChild(plant);
-      gameState.plantArr.push(plant);
-      gameState.readyPlant.length = 0;
-      sodPathEle.removeEventListener('click', placePlant);
-      gameState.bullets.push(createBullet(10, event.offsetX, 1));
+    let plant;
+    if (gameState.readyPlant === "peashooter") {
+      plant = new Peashooter;
+        gameState.sunCount -= 100;
+    } else if (gameState.readyPlant === 'walnut') {
+      plant = new Walnut;
+      gameState.sunCount -= 50;
     }
+    plant.plant.style.left = evt.offsetX - 48 + 'px';
+    sodPathEle.appendChild(plant);
+    gameState.readyPlant = '';
+    sodPathEle.removeEventListener('click', placePlant);
+    sunCountEle.innerText = gameState.sunCount;
   }
 }
 
 
 window.onload = function() {
-  createSuns();
+
   let zombieCreateTimer = setInterval(createZombies, 4000);
 
   let sunCreateTimer = setInterval(createSuns, 6000);
   backgroundEle.addEventListener('click', collectSun);
-  //  gameConsoleEle.addEventListener('drag', dragPlant);
+//  gameConsoleEle.addEventListener('drag', dragPlant);
   gameConsoleEle.addEventListener('click', selectPlant);
 
 }
+let gameBegin = function() {
 
-function createBullet(speed, position, damage) {
-  let bullet = document.createElement('img');
-  bullet.className = 'bullet';
-  bullet.dataset.speed = speed;
-  bullet.style.left = position + 'px';
-  bullet.dataset.damage = damage;
-  bullet.src = "assets/plant-bullet.gif";
-  sodPathEle.appendChild(bullet);
-  return bullet;
 }
-
-let createZombies = function() {
-  let zombie = document.createElement('img');
-  zombie.className = "zombie";
-  zombie.src = "assets/zombie-01.gif";
-  gameState.zombiesArr.push(zombie);
-  console.log(gameState.zombiesArr);
-  zombie.style.left = '1100px'
-  zombie.style.bottom = '14px';
-  sodPathEle.appendChild(zombie);
-
-  let zombieMoveTimer = setInterval(function() {
-    for (var i = 0; i < gameState.zombiesArr.length; i++) {
-      if (gameState.zombiesArr[i].offsetLeft < -50) {
-        clearInterval(zombieMoveTimer);
-        //  window.alert('Zombies Win')
-      } else {
-        gameState.zombiesArr[i].style.left = gameState.zombiesArr[i].offsetLeft - 1 + 'px';
-      }
-    }
-  }, 10)
-};
-
-
-setInterval(function() {
-  for (var i = 0; i < gameState.plantArr.length; i++) {
-    if (parseInt(gameState.plantArr[i].dataset.attack) !== 0) {
-      gameState.bullets.push(createBullet(gameState.plantArr[i].dataset.speed, gameState.plantArr[i].offsetLeft + 25, gameState.plantArr[i].dataset.attack));
-    }
-  }
-}, 3000);
-
-setInterval(function() {
-  for (var i = 0; i < gameState.bullets.length; i++) {
-    gameState.bullets[i].style.left = gameState.bullets[i].offsetLeft + parseInt(gameState.bullets[i].dataset.speed) + "px";
-    for (var j = 0; j < gameState.zombiesArr.length; j++) {
-      if (gameState.bullets[i].offsetLeft + gameState.bullets[i].offsetWidth - 30 >= gameState.zombiesArr[j].offsetLeft) {
-        if (gameState.bullets[i].offsetLeft - gameState.zombiesArr[j].offsetLeft - gameState.zombiesArr[j].offsetWidth < 5) {
-          //  calcDamage(gameState.zombiesArr[j], gameState.bullets[i], 'gif')
-          //  zombiesState(j, gameState.zombiesArr[j], gameState.zombiesArr)
-          gameState.zombiesArr[j].remove();
-          gameState.zombiesArr.splice(j, 1);
-          sodPathEle.removeChild(gameState.bullets[i])
-          gameState.bullets.splice(i, 1);
-          break;
-        }
-        if (gameState.bullets[i].offsetLeft + gameState.bullets[i].offsetWidth > sodPathEle.offsetWidth) {
-          gameState.bullets[i].parentNode.removeChild(gameState.bullets[i]);
-          gameState.bullets.splice(i, 1);
-        }
-      }
-    }
-  }
-}, 200);
